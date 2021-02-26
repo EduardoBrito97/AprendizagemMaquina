fcd29bea137ec92db5b86bd0541cab7c05de5fd2
Eduardo Barreto <eduardobarreto97@gmail.com>
2021-02-26 16:12:46 -0300

    Fix on dataset reader to match the knn

--- Lista1/dataset_reader.py
+++ Lista1/dataset_reader.py
@@ -1,15 +1,14 @@
 import pandas as pd
 from scipy.io import arff
 
-datasets = ["pc1.arff", "cocomo81.arff"]
-dataset_labels = [
-    ['loc', 'v(g)', 'ev(g)', 'iv(g)', 'n', 'v', 'l', 'd', 'i', 'e', 'b', 't', 'lOCode', 'lOComment', 'lOBlank', 'lOCodeAndComment', 'uniq_Op', 'uniq_Opnd', 'total_Op', 'total_Opnd', 'branchCount'],
-    ['rely', 'data', 'cplx', 'time', 'stor', 'virt', 'turn', 'acap', 'aexp', 'pcap', 'vexp', 'lexp', 'modp', 'tool', 'sced', 'loc', 'actual']
-]
-
-number_of_attributes = [len(dataset_labels[0]), len(dataset_labels[1])]
+datasets = ["pc1.arff", "kc1-class-level-defectiveornot.arff"]
+number_of_attributes = [21, 94]
 
 def get_dataset(dataset_index):
     data = arff.loadarff('datasets/' + datasets[dataset_index])
     df = pd.DataFrame(data[0])
+    if dataset_index == 0:
+        df['defects'] = df['defects'].astype(str)
+    elif dataset_index == 1:
+        df['DL'] = df['DL'].astype(str)
     return df
