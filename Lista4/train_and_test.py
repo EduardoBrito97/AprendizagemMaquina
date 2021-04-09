5f6053973077440baee23a9ec62038f2ea465ed6
Eduardo Barreto <eduardobarreto97@gmail.com>
2021-04-09 14:45:48 -0300

    Fix on KMeans and implementing NB

--- Lista4/train_and_test.py
+++ Lista4/train_and_test.py
@@ -7,22 +7,21 @@ from sklearn.metrics import classification_report, confusion_matrix, silhouette_
 from sklearn.naive_bayes import GaussianNB
 from dataset_reader import get_dataset, number_of_attributes
 
-def test(x_train, y_train, x_test, y_test, best_k):
-    y_pred = []
-    y_true = []
-
+kfold_num = 5
+# Pegamos os novos conjuntos gerados pelo kmeans, treinamos o Gaussian NB e predizemos o x_teste
+def test(x_train, y_train, x_test, y_test):
     tp = 0
     fp = 0
     tn = 0
     fn = 0
 
+    gnb = GaussianNB()
+    y_pred = gnb.fit(x_train, y_train).predict(x_test)
+    y_true = []
+
     for index in range(len(x_test)):
-        instance_class = 'true' in y_test[index].lower()
-
-        predicted_class = False
-        if normalized_dist > class_threshold:
-            predicted_class = True
-
+        instance_class = 'true' in y_test.iloc[index].lower()
+        predicted_class = y_pred[index]
         if predicted_class == True and instance_class == True:
             tp = tp + 1
         elif predicted_class == True and instance_class == False:
@@ -32,9 +31,7 @@ def test(x_train, y_train, x_test, y_test, best_k):
         else:
             fn = fn + 1
 
-        y_pred.append(predicted_class)
         y_true.append(instance_class)
-
     return tp, fp, tn, fn, y_pred, y_true
 
 def select_using_elbow_method(distortions):
@@ -83,45 +80,65 @@ def train_and_test(x_train, y_train, x_test, y_test):
     rows_true = []
     rows_false = []
     index = 0
+
+    # Separando o x de teste entre x_true e x_false
     for _, row in y_train.items():
         if 'true' in str(row).lower():
             rows_true.append(index)
         else:
             rows_false.append(index)
         index = index + 1
-    
+
+    # Para cada classe, procuramos o melhor K usando elbow/silhouette
     x_train_true = x_train.iloc[rows_true]
     k_true = find_best_k(x_train_true)
 
     x_train_false = x_train.iloc[rows_false]
     k_false = find_best_k(x_train_false)
 
-    return test(x_train, y_train, x_test, y_test, k)
+    kmeans_true = KMeans(n_clusters=k_true).fit(x_train_true)
+    kmeans_false = KMeans(n_clusters=k_false).fit(x_train_false)
+
+    # Criamos grupos com os clusters pra x e valores corretos para y
+    new_x_train = []
+    new_y_train = []
+
+    for cluster_center in kmeans_true.cluster_centers_:
+        new_x_train.append(cluster_center)
+        new_y_train.append([True])
+
+    for cluster_center in kmeans_false.cluster_centers_:
+        new_x_train.append(cluster_center)
+        new_y_train.append([False])
+
+    # Por fim, testamos usando Gaussian Nayve Bayes usando os novos grupos
+    return test(new_x_train, new_y_train, x_test, y_test)
 
 def gen_reports_and_statistics(dataset, dataset_target, dataset_name, dataset_index, reports, statistics):
-    kfold = StratifiedKFold(n_splits=5, shuffle=True)
+    kfold = StratifiedKFold(n_splits=kfold_num, shuffle=True)
     fold_num = 0
     for train_index, test_index in kfold.split(dataset, dataset_target):
-        train = pc1.loc[train_index,:]
+        train = dataset.iloc[train_index,:]
         x_train = train.iloc[:, :-1]
         y_train = train.iloc[:, number_of_attributes[dataset_index]]
 
-        test = pc1.loc[test_index,:]
+        test = dataset.iloc[test_index,:]
         x_test = test.iloc[:, :-1]
         y_test = test.iloc[:, number_of_attributes[dataset_index]]
 
         tp, fp, tn, fn, y_pred, y_true = train_and_test(x_train, y_train, x_test, y_test)
         report = classification_report(y_true, y_pred)
-        reports['dataset_name'][fold_num] = report
+        reports[dataset_name][fold_num] = report
         
-        statistics['dataset_name'][fold_num] = {}
-        statistics['dataset_name'][fold_num]['tp'] = tp
-        statistics['dataset_name'][fold_num]['fp'] = tp
-        statistics['dataset_name'][fold_num]['tn'] = tn
-        statistics['dataset_name'][fold_num]['fn'] = fn
+        statistics[dataset_name][fold_num] = {}
+        statistics[dataset_name][fold_num]['tp'] = tp
+        statistics[dataset_name][fold_num]['fp'] = tp
+        statistics[dataset_name][fold_num]['tn'] = tn
+        statistics[dataset_name][fold_num]['fn'] = fn
         fold_num = fold_num + 1
 
 def gen_txt(reports, statistics):
+    breakpoint()
     for dataset in reports.keys():
         folder_name = "results/" + dataset + "/" 
         for percentage in reports[dataset]:
